d6b00c85b016d62ba2a1f87c7d84b5c9e6da2ce3
wang.li <2432057666@qq.com>
2024-01-11 17:34:09 +0800

    123445554

--- w.js
+++ w.js
@@ -1,7 +1,7 @@
-console.log('巴拉拉拉啦123----')
+console.log('巴拉拉拉啦123123----')
 
-console.log('lw是小笨蛋')
+console.log('lw是小笨蛋21321')
 
-console.log('yyx是大笨蛋')
+console.log('yyx是大笨蛋12312')
 
 console.log('我是蝴蝶，蝴蝶是我，啦啦啦啦 啦~ ~ ~')
