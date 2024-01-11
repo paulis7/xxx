0476ace651c7bc21d5e12ddda2e373738dcd59eb
paulis <2580699604@qq.com>
2024-01-11 17:47:13 +0800

    1

--- w.js
+++ w.js
@@ -1,7 +1,9 @@
-console.log('巴拉拉拉啦123123----')
+console.log('巴拉拉拉啦----')
 
-console.log('lw是小笨蛋21321')
+console.log('lw是大聪明')
 
-console.log('yyx是大笨蛋12312')
+console.log('yyx是小笨蛋')
 
-console.log('我是蝴蝶，蝴蝶是我，啦啦啦啦 啦~ ~ ~')
+console.log('我是蝴蝶，蝴蝶是我，啦啦啦啦啦~ ~ ~')
+
+console.log('你小子也太可爱了吧，敲！')
